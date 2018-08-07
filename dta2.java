bc33c8041bdeca91edb4d2e895d2e9abf2ebc38a
shreyajainn <32064863+shreyajainn@users.noreply.github.com>
2018-08-07 11:14:30 +0530

    Add files via upload

---
+++ dta2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class dta2
+{
+public static void main(String a[])
+{
+for(int i=0;i<a.length;i++)
+System.out.println(a[i]);
+}
+}
---
+++ op.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class op
+{
+public static void main(String a[]) throws IOException
+{
+BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+String s = in.readLine();
+System.out.println(s);
+}
+}
